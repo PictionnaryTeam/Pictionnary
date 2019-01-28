02564229dabc429580d2ca6a3dd93fa0ccafd50c
Christophe Roulin <roulinch@etml.educanet2.ch>
2019-01-28 09:29:23 +0100

    Add TCP server and TCP client

---
+++ Code/Pictionnary/ConnectionTest/Managers/PacketManager.cs
@@ -0,0 +1,33 @@
+﻿using ConnectionTest.Handlers;
+using ConnectionTest.Objects;
+using ConnectionTest.Objects.Packets;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConnectionTest.Managers
+{
+    class PacketManager
+    {
+        Dictionary<PacketType, PacketReceiveHandler> packetHandlers;
+
+        public PacketManager()
+        {
+            packetHandlers = new Dictionary<PacketType, PacketReceiveHandler>();
+
+            AddHandler(PacketType.CLIENTPACKETPING, new PingPacketHandler());
+        }
+
+        public void AddHandler(PacketType packetType, PacketReceiveHandler handler)
+        {
+            packetHandlers.Add(packetType, handler);
+        }
+
+        public Packet HandleReceivedPacket(Packet packet)
+        {
+            return packetHandlers[packet.PacketType].OnPacketReceive(packet);
+        }
+    }
+}
---
+++ Code/Pictionnary/ConnectionTest/Objects/Packets/Client/ClientPingPacket.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConnectionTest.Objects.Packets.Client
+{
+    [Serializable]
+    class ClientPingPacket : Packet
+    {
+        public ClientPingPacket() : base(PacketType.CLIENTPACKETPING)
+        {
+        }
+    }
+}
