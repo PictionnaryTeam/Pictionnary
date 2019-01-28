d67a64308d74f098aca2ba603cf6cfde7e618100
lopezes <lopezes@etml.educanet2.ch>
2019-01-28 11:24:35 +0100

    Forms du programme selon les maquettes

---
+++ Code/Prototype-SelonMaquettes/Prototype-SelonMaquettes/CreateRoomView.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Prototype_SelonMaquettes
+{
+    public partial class CreateRoomView : Form
+    {
+        public CreateRoomView()
+        {
+            InitializeComponent();
+            Icon = Prototype_SelonMaquettes.Properties.Resources.Icon;
+        }
+    }
+}
