f3e30b9b16c531f7f6b24bcb12f4756fafc7b6af
Léa Cherpillod <lea@cherpillod.com>
2019-02-08 14:27:53 +0100

    Finish string extensions - Léa

--- Code/Pictionnary/Pictionnary/Other/StringExt.cs
+++ Code/Pictionnary/Pictionnary/Other/StringExt.cs
@@ -9,14 +9,28 @@ using System.Text;
 
 namespace IndexatorusConnector.Helper.Extension
 {
+    /// <summary>
+    /// The string extensions class
+    /// </summary>
     public static class StringExt
     {
+        /// <summary>
+        /// Refactor a text by putting it into lower, removing extra spaces at the beginning or the end, diacritics, numbers and special chars except '-'
+        /// </summary>
+        /// <param name="text">The text to refactor</param>
+        /// <returns>The refactored text</returns>
+        public static string RefactorText(this string text)
+        {
+            //Return the refactored text
+            return text.ToLower().RemoveDiacritics().RemoveStartAndEndSpaces().RemoveSpecialChars().RemoveNumbers();
+        }
+
         /// <summary>
         /// Remove the diacritics from a string
         /// </summary>
         /// <param name="text">The text to modify</param>
         /// <returns>The text without the diacritics</returns>
-        public static string RemoveDiacritics(this string text)
+        private static string RemoveDiacritics(this string text)
         {
             //If the string is null or only contains spaces
             if (string.IsNullOrWhiteSpace(text))
@@ -31,18 +45,13 @@ namespace IndexatorusConnector.Helper.Extension
             //Return the text by assembling the chars without their diacritics
             return new string(chars).Normalize(NormalizationForm.FormC);
         }
-        
+
         /// <summary>
-        /// Remove the special chars at the end of the string
+        /// Remove the spaces at the beginning and end of the string
         /// </summary>
         /// <param name="text">The text to modify</param>
-        /// <returns>The text without special chars at the end</returns>
-        public static string RemoveEnd(this string text)
-        {
-            return text;
-        }
-
-        public static string RemoveStartAndEndSpaces(this string text)
+        /// <returns>The modified text</returns>
+        private static string RemoveStartAndEndSpaces(this string text)
         {
             //While the first char is a space
             while (text[0] == ' ')
@@ -52,8 +61,55 @@ namespace IndexatorusConnector.Helper.Extension
             }
 
             //While the last char is a space
+            while (text[text.Length - 1] == ' ')
+            {
+                //Remove it
+                text.Substring(0, text.Length - 1);
+            }
 
             return text;
+        }
+        
+        /// <summary>
+        /// Remove the special chars of the string except '-' 
+        /// </summary>
+        /// <param name="text">The text to modify</param>
+        /// <returns>The text without special chars</returns>
+        private static string RemoveSpecialChars(this string text)
+        {
+            StringBuilder newText = new StringBuilder();
+
+            foreach (char chr in text)
+            {
+                //Check if the char is a letter or a digit, a - or a space
+                if (Char.IsLetterOrDigit(chr) || chr == '-' || chr == ' ')
+                    //If it is append it
+                    newText.Append(chr);
+            }
+
+            //Return the stringbuilder as a string
+            return newText.ToString();
+        }
+        
+        /// <summary>
+        /// Remove the numbers from a string
+        /// </summary>
+        /// <param name="text">The text to modify</param>
+        /// <returns>The modified text</returns>
+        private static string RemoveNumbers(this string text)
+        {
+            StringBuilder newText = new StringBuilder();
+
+            foreach (char chr in text)
+            {
+                //Checi if the char is a letter or a digit, a - or a space
+                if (!Char.IsDigit(chr))
+                    //If it is append it
+                    newText.Append(chr);
+            }
+
+            //Return the stringbuilder as a string
+            return newText.ToString();
         }
     }
 }
