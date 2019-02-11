737da00e7e1e96c2afa9bc2acfee8b1a34f10c26
Ministoud <stouderra@etml.educanet2.ch>
2019-02-11 10:01:46 +0100

    résolution de bug

--- Code/PrototypeDessin/PrototypeDessin/PanelRenderer.cs
+++ Code/PrototypeDessin/PrototypeDessin/PanelRenderer.cs
@@ -22,16 +22,39 @@ namespace PrototypeDessin
             List<Line> lines = drawingToRender.Lines;
             foreach (Line currentLine in lines)
             {
-                foreach (Pixel currentPixel in currentLine.Pixels)
+                /*foreach (Pixel currentPixel in currentLine.Pixels)
                 {
-                    paintEventArgs.Graphics.(new Pen(currentLine.LineColor), currentPixel.X, currentPixel.Y, currentLine.BrushSize, currentLine.BrushSize);
-                }
+                    paintEventArgs.Graphics.DrawEllipse(new Pen(currentLine.LineColor), currentPixel.X, currentPixel.Y, currentLine.BrushSize, currentLine.BrushSize);
+                }*/
 
                 /*for(int i = 0; i < lines.Count; ++i)
                     {
 
                     }*/
-                }
+
+                //int tempX = 0;
+                //int tempY = 0;
+                //bool isFirstLine = true;
+
+                //foreach (Pixel currentPixel in currentLine.Pixels)
+                //{
+                //    if (isFirstLine)
+                //    {
+                //        tempX = currentPixel.X;
+                //        tempY = currentPixel.Y;
+                //        isFirstLine = false;
+                //    }
+
+                //    paintEventArgs.Graphics.DrawLine(new Pen(currentLine.LineColor, currentLine.BrushSize), tempX, tempY, currentPixel.X, currentPixel.Y);
+                //    tempX = currentPixel.X;
+                //    tempY = currentPixel.Y;
+                //}
+
+                List<PointF> points = new List<PointF>();
+                foreach (Pixel p in currentLine.Pixels) points.Add(new PointF(p.X, p.Y));
+
+                paintEventArgs.Graphics.DrawLines(new Pen(currentLine.LineColor, currentLine.BrushSize), points.ToArray());
             }
+        }
     }
 }
