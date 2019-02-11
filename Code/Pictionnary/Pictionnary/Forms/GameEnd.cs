54143495825e90590c632a15e13e873437cf918c
Boris Hutzli <hutzlibo@etml.educanet2.ch>
2019-02-11 09:21:25 +0100

    Ajout de commentaires - Boris

--- Code/Pictionnary/Pictionnary/Forms/GameEnd.cs
+++ Code/Pictionnary/Pictionnary/Forms/GameEnd.cs
@@ -18,21 +18,24 @@ namespace Pictionnary.Forms
         }
 
         /// <summary>
-        /// Displays the players
+        /// Displays the players' name and points
         /// </summary>
-        /// <param name="first"></param>
-        /// <param name="second"></param>
-        /// <param name="third"></param>
+        /// <param name="first">First Player</param>
+        /// <param name="second">Second Player</param>
+        /// <param name="third">Third Player</param>
         public void ShowWinners(Player first, Player second, Player third)
         {
+            // Shows the first player info
             lblFirstPlace.Text = $"{first.Name} :";
             lblFirstPoints.Text = $"{first.Points.ToString()} pts";
             lblFirstPodium.Text = first.Name;
 
+            // Shows the second player info
             lblSecondPlace.Text = $"{second.Name} :";
             lblSecondPoints.Text = $"{second.Points.ToString()} pts";
             lblSecondPodium.Text = second.Name;
 
+            // Shows the third player info
             lblThirdPlace.Text = $"{third.Name} :";
             lblThirdPoints.Text = $"{third.Points.ToString()} pts";
             lblThirdPodium.Text = third.Name;
