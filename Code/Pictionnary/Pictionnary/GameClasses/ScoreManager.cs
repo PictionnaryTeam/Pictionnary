45f9fc1e0e43b65f1710ce893cabee1bc57abb88
Christophe Roulin <roulinch@etml.educanet2.ch>
2019-03-04 08:35:11 +0100

    Ajout de la vérification du mot avant de l'écrire dans le chat et ajout d'une classe permettant une gestion des points - roulinch

---
+++ Code/Pictionnary/Pictionnary/GameClasses/ScoreManager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pictionnary.GameClasses
+{
+    class ScoreManager
+    {
+        private static ScoreManager _instance;
+
+
+        private ScoreManager()
+        {
+
+        }
+
+        public static ScoreManager GetInstance()
+        {
+            if (_instance == null)
+                _instance = new ScoreManager();
+
+
+            return _instance;
+        }
+
+
+        public void PlayerFoundWord(string playerName, DateTime time)
+        {
+
+        }
+    }
+}
